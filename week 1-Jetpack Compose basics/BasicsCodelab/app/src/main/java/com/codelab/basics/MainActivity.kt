3f159c2b0074acf5e55cad6b2d830792ca1f81f0
feelpass <youngpil.lee@gmail.com>
2022-01-15 01:33:16 +0900

    1주차

--- week 1-Jetpack Compose basics/BasicsCodelab/app/src/main/java/com/codelab/basics/MainActivity.kt
+++ week 1-Jetpack Compose basics/BasicsCodelab/app/src/main/java/com/codelab/basics/MainActivity.kt
@@ -27,14 +27,16 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.Icons.Filled
 import androidx.compose.material.icons.filled.ExpandLess
 import androidx.compose.material.icons.filled.ExpandMore
+import androidx.compose.material.icons.filled.Share
 import androidx.compose.runtime.*
-import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
@@ -86,6 +88,18 @@ fun HelloContent(name: String, onNameChange: (String) -> Unit) {
         OutlinedTextField(value = name, onValueChange = onNameChange, label = {
             Text(text = "이름")
         })
+        TextButton(onClick = {
+
+        }
+
+        ) {
+            Icon(
+                painter = painterResource(id = R.drawable.ic_launcher_background),
+                contentDescription = "",
+                modifier = Modifier.padding(end = 8.dp)
+            )
+            Text(text = "아웃라인버튼")
+        }
     }
 }
 
